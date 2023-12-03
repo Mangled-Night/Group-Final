9a607c04d25fedbac44304dc4157eb14f8d5887c
Mangled-Night <daniel.dewar04@gmail.com>
2023-12-03 00:46:00 -0500

    Cleans Audio Files of Metadata Displays Waveform in Chunks instead of as a whole Thinking about how to let tinker automate interating through the chunks until user input

--- controller.py
+++ controller.py
@@ -1,22 +1,37 @@
-import numpy
-import matplotlib as Mplt
+import tempfile
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.io import wavfile as sio
 from pydub import AudioSegment
-
+import taglib
+import math
 
 class Controller:
     _file = None
+    _channel = None
+    _tags = []
 
     def LoadFile(self, uFile):
+        print(uFile)
         parsed_file = uFile.split(".")
         if(parsed_file[-1] == "wav"):
+            self.ClearMeta(uFile)
             self._file = uFile
         else:
             try:
+                self.ClearMeta(uFile)
                 audio = AudioSegment.from_file(uFile)
+                name = uFile.split(".")[0] + ".wav"
                 self._file = audio.export(format="wav")
             except Exception as err:
                 print(f'An error occured "{err}"')
+            else:
+                samplerate, data = sio.read(self._file)
+                self._channels = data.shape[len(data.shape) - 1]
+
+    def ClearMeta(self, file):
+        with taglib.File(file, save_on_exit=True) as audio:
+            audio.tags.clear()
 
     def ShowWav(self):
         samplerate, data = sio.read(self._file)
@@ -24,11 +39,32 @@ class Controller:
         print(f"sample rate = {samplerate}Hz")
         length = data.shape[0] / samplerate
         print(f"length = {length}s")
+        print(data.shape, data.dtype, samplerate)
+
+        print(data[0:20][:, 0])
+        print(data[:, 0])
+        print(data[:, 1])
+
+        n = 10
+        for x in range(0, n):
+            start = math.ceil(data.shape[0] * x/n)
+            end = math.ceil(data.shape[0] * (x+1)/n)
+            l_start = length * x/n
+            l_end = length * (x+1)/n
+            time = np.linspace(l_start, l_end , (end-start))
+            plt.plot(time, data[start:end][:, 0], label="Left channel")
+            plt.plot(time, data[start:end][:, 1], label="Right channel")
+            plt.legend()
+            plt.xlabel("Time [s]")
+            plt.ylabel("Amplitude")
+            plt.show()
+
 
 
 def main():
     C = Controller()
-    C.LoadFile("")
+    C.LoadFile("Sample2.wav")
+    C.ShowWav()
 
 
 
