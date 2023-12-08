8c529081c6cdf5c5a4d6c19f2d7059f3f88064a8
Mangled-Night <daniel.dewar04@gmail.com>
2023-12-08 14:36:57 -0500

    Modified the Displaty function to allow user to enter 3 extra frequencies along with 3 default frequencies

--- model.py
+++ model.py
@@ -40,8 +40,8 @@ class Controller:
             #If user entered nothing or entered a value too great
 
         #Sets the bounds
-        d_start = math.ceil(self._samplerate * start)
-        d_end = math.ceil(self._samplerate * end)
+        d_start = int(self._samplerate * start)
+        d_end = int(self._samplerate * end)
 
         if(self._channels == 1):
             # Displays the Data
@@ -62,7 +62,7 @@ class Controller:
             plt.ylabel("Amplitude")
             plt.show()
 
-    def Frequency(self):
+    def Frequency(self, u_frequencies):
         if(self._channels == 1):
             spectrum1, freqs1, t1, im1 = plt.specgram(self._data, Fs=self._samplerate, NFFT=1024, cmap=plt.get_cmap('autumn'))
             cbar1 = plt.colorbar(im1)
@@ -89,29 +89,13 @@ class Controller:
 
             plt.show()
 
-        self.RT60(freqs1, spectrum1, t1, 1000)
-        self.RT60(freqs1, spectrum1, t1, 2000)
-        self.RT60(freqs1, spectrum1, t1, 11218)
+        self.RT60(freqs1, spectrum1, t1, u_frequencies)
 
 
-
-    def RT60(self, freqs, spectrum, t, target_frequency):
-        def frequency_check(target_frequency):
-            ratio = spectrum.shape[0]/freqs.max()
-            data_for_frequency = spectrum[math.ceil(ratio * target_frequency)]
-
-            data_in_db_fun = 10 * np.log10(data_for_frequency)
-
-            idx_last_pos = 0
-            for x in range(len(data_in_db_fun)):
-                if(data_in_db_fun[x] > 0):
-                    idx_last_pos = x
-            pos_data_in_db = data_in_db_fun[:idx_last_pos+1]
-            return pos_data_in_db, idx_last_pos
-
+    def RT60(self, freqs, spectrum, t, user_frequencies):
+        ratio = spectrum.shape[0] / freqs.max()
         def HeighestFrequency():
             _max = len(freqs)
-            ratio = spectrum.shape[0]/freqs.max()
             heighest_freq = None
             heighest_plottable_freq = None
             for x in range(1, _max):
@@ -124,70 +108,127 @@ class Controller:
                 if (len(_data[:idx_last_pos + 1]) > 1 and heighest_freq == None):
                     heighest_freq = (_max - x) / ratio
 
-                if(len(_data[:idx_last_pos + 1]) >= 11):
+                if (len(_data[:idx_last_pos + 1]) >= 11):
                     heighest_plottable_freq = (_max - x) / ratio
                     break
 
             return heighest_freq, heighest_plottable_freq
 
+        heightest_frequency , heighest_plottable = HeighestFrequency()
 
+        print(f'Heighest audiable frequency is {int(heightest_frequency)}Hz')
+        print(f'Heighest Plottable frequency is {int(heighest_plottable)}')
 
-        data_in_db,last_pos = frequency_check(target_frequency)
+        default_frequencies = [0, int(heighest_plottable/2), int(heighest_plottable)]
 
-
-        plt.figure(2)
-
-        plt.plot(t[:last_pos+1], data_in_db, linewidth=1, alpha=.7, color='#004bc6')
-
-        plt.xlabel('Time (s)')
-
-        plt.ylabel('Power (db)')
-
-        plt.title(f'{target_frequency}Hz')
-
-        index_of_max = np.argmax(data_in_db)
-        value_of_max = data_in_db[index_of_max]
-        plt.plot(t[index_of_max], data_in_db[index_of_max], 'go', label="Max")
-
-        sliced_array = data_in_db[index_of_max:]
-        value_of_max_less_5 = value_of_max - 5
-
-        def find_nearest_value(array, value):
-            array = np.asarray(array)
-            idx = (np.abs(array - value)).argmin()
-
-            return array[idx]
-
-        value_of_max_less_25 = value_of_max - 25
-        value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
-        index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
-        plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro', label='Max-25DB')
-
-        sliced_array = data_in_db[index_of_max : index_of_max_less_25[0][0]]
-        value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
-        index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)
-        plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo', label='Max-5DB')
+        for x in user_frequencies:
+            if (x > freqs[len(freqs) - 1]):
+                print(f'Outside Max Frequency Recoreded. Cannot Plot at {x}')
+                user_frequencies.remove(x)
+            elif (x > heighest_plottable):
+                print(f'Not enough data to plot at {x}Hz')
+                user_frequencies.remove(x)
 
 
 
-        rt20 = (t[index_of_max_less_5] - t[index_of_max_less_25])
 
-        rt60 = 3 * rt20
+        def plot_frequencies(target_frequency, can_label, _color):
+            def frequency_check(target_frequency):
+                data_for_frequency = spectrum[int(ratio * target_frequency)]
+
+                data_in_db_fun = 10 * np.log10(data_for_frequency)
+
+                idx_last_pos = 0
+                for x in range(len(data_in_db_fun)):
+                    if(data_in_db_fun[x] > 0):
+                        idx_last_pos = x
+                pos_data_in_db = data_in_db_fun[:idx_last_pos+1]
+                return pos_data_in_db, idx_last_pos
+
+            data_in_db,last_pos = frequency_check(target_frequency)
+
+
+            plt.figure(2)
+
+            plt.plot(t[:last_pos+1], data_in_db, linewidth=1, alpha=.7, color=_color, label=f'{target_frequency}Hz')
+
+
+
+            plt.xlabel('Time (s)')
+
+            plt.ylabel('Power (db)')
+
+            #plt.title(f'{target_frequency}Hz')
+
+            index_of_max = np.argmax(data_in_db)
+            value_of_max = data_in_db[index_of_max]
+
+            sliced_array = data_in_db[index_of_max:]
+            value_of_max_less_5 = value_of_max - 5
+
+            def find_nearest_value(array, value):
+                array = np.asarray(array)
+                idx = (np.abs(array - value)).argmin()
+
+                return array[idx]
+
+            value_of_max_less_25 = value_of_max - 25
+            value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
+            index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
+
+
+            sliced_array = data_in_db[index_of_max : index_of_max_less_25[0][0]]
+            value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
+            index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)
+
+            if(can_label):
+                plt.plot(t[index_of_max], data_in_db[index_of_max], 'go', label="Max")
+                plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro', label='Max-25DB')
+                plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo', label='Max-5DB')
+            else:
+                plt.plot(t[index_of_max], data_in_db[index_of_max], 'go')
+                plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro')
+                plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo')
+
+
+            rt20 = (t[index_of_max_less_5] - t[index_of_max_less_25])
+
+            rt60 = 3 * rt20
+
+
+            print(f'The RT60 reverb time at freq {int(target_frequency)}Hz is {np.round(abs(rt60), 2)[0]} seconds')
+            print(f'Amount of audiable sound in this frequency is about {np.round(t[last_pos] - t[0], 2)}s')
+
+
+        colors = ["Red", "Blue", "Black"]
+
+        for x in range(len(default_frequencies)):
+            if (x == len(default_frequencies) - 1):
+                plot_frequencies(default_frequencies[x], True, colors[x])
+            else:
+                plot_frequencies(default_frequencies[x], False, colors[x])
 
         plt.grid()
         plt.legend()
         plt.show()
 
-        print(f'The RT60 reverb time at freq {int(target_frequency)}Hz is {np.round(abs(rt60), 2)[0]} seconds')
-        print(f'Amount of audiable sound in this frequency is about {np.round(t[last_pos] - t[0], 2)}s')
-        print(f'Heighest audiable frequency is {math.floor(HeighestFrequency()[0])}Hz')
-        print(f'Heighest Plottable frequency is {math.floor(HeighestFrequency()[1])}')
+        for x in range(len(user_frequencies)):
+            if(x == len(user_frequencies)-1):
+                plot_frequencies(user_frequencies[x], True, colors[x])
+            else:
+                plot_frequencies(user_frequencies[x], False, colors[x])
+
+        plt.grid()
+        plt.legend()
+        plt.show()
+
+
 
 def main():
     C = Controller()
     C.LoadFile("Sample5.wav")
-    C.ShowWav(0)
-    C.Frequency()
+    #C.ShowWav(0)
+    C.Frequency([1000, 2000, 50000])
 
 
 main()
