31374a81a916b0eca1a3bd48fdf34de6317bfdf3
Pierson <piersonalexander144@gmail.com>
2023-12-12 20:08:56 -0500

    Returned functions

--- newController.py
+++ newController.py
@@ -5,18 +5,21 @@ class AudioController:
     def __init__(self):
         self._model = Model()
 
+
     def load_file(self, u_file):
         self._model.LoadFile(u_file)
 
+
     def show_wav(self,start, end):
         self._model.ShowWav(start,end)
+        return self._model.ShowWav()
 
     def frequency(self,s_freqs=None):
         self._model.Frequency(s_freqs)
-
+        return self._model.Frequency(s_freqs)
     def returnStats(self):
         self.returnStats()
-
+        return self.returnStats()
 
 
 
